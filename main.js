3a28d8d75908c40ac24b6b12543fe36e69e5e309
RayhaanT <tanweer.rayhaan@gmail.com>
2019-04-24 20:28:32 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var text
+var clicks = 0
+var fontSize = 10
+
+window.onload = function() {
+    text = document.getElementById("text")
+    text.style.fontSize = fontSize + "px"
+    document.addEventListener('mousedown', docClicked)
+}
+
+function docClicked(evt) {
+    console.log('y')
+    clicks++
+    fontSize++
+    text.style.fontSize = fontSize + "px"
+    if(clicks > 10) {
+        text.innerHTML = "Bonjour le monde"
+    }
+}
