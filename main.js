49540b5c952012c7e819ffc9c4ef3fe46045434d
RayhaanT <tanweer.rayhaan@gmail.com>
2019-04-25 18:09:51 -0400

    Centred and added translations

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var text
+var langClicks = 0
 var clicks = 0
 var fontSize = 10
 var degreeInterval = 10
 var set = true
 var degrees = 10
+var translations = ["Bonjour le monde", "Hallo wereld", "你好世界", "Hallo Welt", "γειά σου κόσμος", "Ciao mondo", "こんにちは世界", "여보세요 세계", "Olá mundo", "Здравствуй, мир", "Hola mundo", "Hello World"]
+var index = 0
 
 window.onload = function() {
     text = document.getElementById("text")
@@ -11,24 +14,29 @@ window.onload = function() {
     //text.style.top = window.innerHeight / 2
     //text.style.left = window.innerWidth / 2
     text.style.fontSize = fontSize + "px"
+    text.style.margin = -text.clientHeight / 2 + "px 0px 0px " + -text.clientWidth / 2 + "px"
     document.addEventListener('mousedown', docClicked)
 }
 
 function docClicked(evt) {
     clicks++
+    langClicks++
     fontSize+=2
     text.style.fontSize = fontSize + "px"
-    if(clicks > 5) {
+    if(clicks >= 5) {
         if(set) {
         setInterval(function () {
         text.style.transform = "rotate(" + degrees + "deg)";
-        degrees+=degreeInterval/60
+        degrees+=degreeInterval/180
     }, 1000/60)
         }
         
-        degreeInterval+=4
+        degreeInterval = (degreeInterval + 1)%40
     }
-    if(clicks > 10) {
-        text.innerHTML = "Bonjour le monde"
+    if(langClicks >= 10) {
+        text.innerHTML = translations[index]
+        langClicks = 0
+        index = (index+1)%12
     }
+    text.style.margin = -text.clientHeight / 2 + "px 0px 0px " + -text.clientWidth / 2 + "px"
 }
