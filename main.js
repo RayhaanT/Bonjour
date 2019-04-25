cfc3baff694fa790d3b189d4308b3333915c2c79
RayhaanT <tanweer.rayhaan@gmail.com>
2019-04-24 21:05:47 -0400

    Rotates

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 var text
 var clicks = 0
 var fontSize = 10
+var degrees = 1
 
 window.onload = function() {
     text = document.getElementById("text")
+    text.style.position = "absolute"
+    //text.style.top = window.innerHeight / 2
+    //text.style.left = window.innerWidth / 2
     text.style.fontSize = fontSize + "px"
     document.addEventListener('mousedown', docClicked)
 }
 
 function docClicked(evt) {
     clicks++
-    fontSize++
+    fontSize+=2
     text.style.fontSize = fontSize + "px"
+    if(clicks > 5) {
+        text.style.transform = "rotate(" + degrees + "deg)";
+        degrees += 10
+    }
     if(clicks > 10) {
         text.innerHTML = "Bonjour le monde"
     }
